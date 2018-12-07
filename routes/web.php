00743e44a6e822ede5f1e03ad4f526648eb3ab83
Ruben <contact@rubenmadila.com>
2018-12-07 15:40:46 +0000

    Changes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@
  * Date: 02/12/2018
  * Time: 10:12
  */
+
+// Authentication Routes...
+Route::group(['middleware' => ['web']], function () {
+    Route::get('login', 'OllieFordandCo\GateKeeper\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'OllieFordandCo\GateKeeper\Http\Controllers\Auth\LoginController@login');
+    Route::post('logout', 'OllieFordandCo\GateKeeper\Http\Controllers\Auth\LoginController@logout')->name('logout');
+
+    // Registration Routes...
+    //if (config ?? true) {
+        Route::get('register', 'OllieFordandCo\GateKeeper\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
+        Route::post('register', 'OllieFordandCo\GateKeeper\Http\Controllers\Auth\RegisterController@register');
+    //}
+});
